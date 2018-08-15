a4489b7f91704ebc5d3aa592c965b5c33554e5ab
swgoodman <spencerwgoodman@icloud.com>
2018-08-15 13:47:46 -0700

    connected routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+  resources :users
+  resources :companies
+  resources :leads
+
+  get '/signin', to: 'sessions#new'
+  post '/sessions/create', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+
 end
