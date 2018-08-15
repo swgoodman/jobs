88dbab64447eef23f8d2c2227c03902980d83373
swgoodman <spencerwgoodman@icloud.com>
2018-08-15 16:14:21 -0700

    set up create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,24 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.create(user_params)
+    if user.save
+      redirect_to user_path(user)
+    else
+      render :new
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :username,
+      :email,
+      :password,
+      :bio,
+      :profile_picture,
+      :created_on
+      )
+    end
 end
