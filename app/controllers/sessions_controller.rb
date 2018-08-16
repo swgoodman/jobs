6ef72cad09859b452583b810bbb4bf431ffe949e
swgoodman <spencerwgoodman@icloud.com>
2018-08-16 10:46:09 -0700

    fixed session destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,22 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
+    @users = User.all
   end
 
   def create
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def destroy
+    session.delete :user_id
+    redirect_to root_url
   end
 end
