4de7bb30ac531d5e4ef2214d01cd3c103bdde1a6
swgoodman <spencerwgoodman@icloud.com>
2018-08-16 13:16:21 -0700

    working on new lead functionality

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -5,6 +5,7 @@ class LeadsController < ApplicationController
   end
 
   def new
+    @lead = Lead.new(user_id: params[:user_id])
   end
 
   def create
@@ -16,5 +17,9 @@ class LeadsController < ApplicationController
   helper_method :set_user
     def set_user
       @user = User.find(session[:user_id])
+    end
+
+    def post_params
+      params.require(:lead).permit(:company, :position, :user_id)
     end
 end
