75392f96beb0a71ae91c556e5daf981a643142d5
swgoodman <spencerwgoodman@icloud.com>
2018-08-29 15:30:07 -0700

    working on site CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,14 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('nav ul').slideToggle(500);
+  });//end slide toggle
+
+  $(window).resize(function() {
+		if (  $(window).width() > 500 ) {
+			$('nav ul').removeAttr('style');
+		 }
+	});//end resize
+});//end ready
